1472e4a82f6ba71678084442623bbe47f3f8c725
Oleksy Lykov <lukov_jr@ukr.net>
2023-05-22 17:46:52 +0300

    done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,80 +1,67 @@
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 import { Contacts } from './Contacts';
 import { Filter } from './Filter';
 import { Forma } from './Forma';
 import { nanoid } from 'nanoid';
 import {Container} from "./App.styled"
 
-export class App extends Component {
-  state = {
-    contacts: [ 
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  ],
-    filter: '',
-  };
+export const App = () => {
+  const [contacts, setContacts] = useState(JSON.parse(window.localStorage.getItem('contacts')) ?? 
+  [
+  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+])
+    const [filter, setFilter] = useState('');
+
+        
+    useEffect((prevState) =>{
+      if(prevState !== contacts) {
+            const localString = JSON.stringify(contacts)
+           window.localStorage.setItem('contacts', localString)
+      }
+    }, [contacts])
 
 
-  componentDidMount() {
-    const contactsInLocalStorage = localStorage.getItem('contacts');
-    const parsedContacrs = JSON.parse(contactsInLocalStorage)
-
-    if(parsedContacrs) {
-      this.setState({contacts: parsedContacrs})
-    }
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-    if (this.state.contacts !== prevState.contacts) {
-      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
-    }
-  }
-
-
-  getVisibleContacts = () => {
-    const normalizedFilter = this.state.filter.toLowerCase();
-    return this.state.contacts.filter(contact =>
+  const getVisibleContacts = () => {
+    const normalizedFilter = filter.toLowerCase();
+    return contacts.filter(contact =>
       contact.name.toLowerCase().includes(normalizedFilter)
     );
   };
 
-  changeFilter = event => {
-    this.setState({ filter: event.currentTarget.value });
+  const changeFilter = event => {
+    setFilter(event.currentTarget.value);
   };
 
-  addContacts = (name, number) => {
+  const addContacts = (name, number) => {
     const contact = {
       id: nanoid(),
       name,
       number,
     };
-    this.setState(pravState => {
-      return { contacts: [contact, ...pravState.contacts] };
-    });
+    setContacts(pravState => [contact, ...pravState]);
   };
 
-  deleteContact = (contactId) => {
-    this.setState(prevState =>({contacts: prevState.contacts.filter(contact => contact.id !== contactId)})
-      
-    )
-  };
-
-  render() {
-    const visiblContacts = this.getVisibleContacts();
-
-    return (
-      <Container>
-        <h1>Phonebook</h1>
-        <Forma onSubmit={this.addContacts} arr={this.state.contacts} />
-        <h2>Contacts</h2>
-        <Filter value={this.state.filter} onChange={this.changeFilter} />
-        <Contacts
-          contacts={visiblContacts}
-          onDeleteContacts={this.deleteContact}
-        />
-      </Container>
+  const deleteContact = contactId => {
+    setContacts(prevState =>
+      prevState.filter(contact => contact.id !== contactId)
     );
-  }
-}
+  };
+
+
+  const visiblContacts = getVisibleContacts();
+
+  return (
+    <Container>
+      <h1>Phonebook</h1>
+      <Forma onSubmit={addContacts} arr={contacts} />
+      <h2>Contacts</h2>
+      <Filter value={filter} onChange={changeFilter} />
+      <Contacts contacts={visiblContacts} onDeleteContacts={deleteContact} />
+    </Container>
+  );
+};
+
+
--- src/components/Forma/Forma.jsx
+++ src/components/Forma/Forma.jsx
@@ -1,70 +1,74 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import PropTypes from 'prop-types';
 import {Input,FormBtn} from "./Forma.styled";
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
-export class Forma extends Component {
-  state = {
-    name: '',
-    number: '',
-  };
+export const Forma = ({ arr, onSubmit }) => {
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
 
-  hendleSubmit = event => {
+  const hendleSubmit = event => {
     event.preventDefault();
-    const nameContacts = this.props.arr.map(el => el.name.toLowerCase());
-    if (nameContacts.includes(this.state.name.toLowerCase())) {
-      Notify.info(`${this.state.name} is in your contacts`);
+    const nameContacts = arr.map(el => el.name.toLowerCase());
+    if (nameContacts.includes(name.toLowerCase())) {
+      Notify.info(`${name} is in your contacts`);
     } else {
-      this.props.onSubmit(this.state.name, this.state.number);
-      this.reset();
+      onSubmit(name, number);
+      reset();
     }
   };
 
-  hendleNameTelChange = event => {
+  const hendleNameTelChange = event => {
     const { name, value } = event.currentTarget;
-    this.setState({ [name]: value });
+    if (name === 'name') setName(value);
+    if (name === 'number') setNumber(value);
   };
 
-  reset = () => {
-    this.setState({ name: '', number: '' });
+  const reset = () => {
+    setName('');
+    setNumber('');
   };
 
-  render() {
-    return (
-      <>
-        <form onSubmit={this.hendleSubmit}>
-          <label>
-            Name
-            <Input
-              type="text"
-              name="name"
-              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-              title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-              required
-              value={this.state.name}
-              onChange={this.hendleNameTelChange}
-            />
-          </label>
+  return (
+    <>
+      <form onSubmit={hendleSubmit}>
+        <label>
+          Name
+          <Input
+            type="text"
+            name="name"
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            required
+            value={name}
+            onChange={hendleNameTelChange}
+          />
+        </label>
 
-          <label>
-            Number
-            <Input
-              type="tel"
-              name="number"
-              pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-              title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-              required
-              value={this.state.number}
-              onChange={this.hendleNameTelChange}
-            />
-          </label>
-          <FormBtn type="submit">Add contact</FormBtn>
-        </form>
-      </>
-    );
-  }
-}
+        <label>
+          Number
+          <Input
+            type="tel"
+            name="number"
+            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+            required
+            value={number}
+            onChange={hendleNameTelChange}
+          />
+        </label>
+        <FormBtn type="submit">Add contact</FormBtn>
+      </form>
+    </>
+  );
+};
 
 Forma.propTypes = {
   onSubmit: PropTypes.func.isRequired,
+  arr: PropTypes.arrayOf(
+    PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      number: PropTypes.string.isRequired,
+    })
+  ),
 };
